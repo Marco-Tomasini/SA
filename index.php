15f9906d34666a29b877bfbaf3de716e562ecacf
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-09-18 12:29:35 -0300

    UPDATE - Primeira versão do Front-end da tela de login

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include __DIR__ . '/public/db.php';
 session_start();
 
 $msg = '';
-if($_SERVER["REQUEST_METHOD"] === 'POST'){
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $email = $_POST['email'] ?? "";
     $pass = $_POST['password'] ?? "";
 
@@ -15,7 +15,7 @@ if($_SERVER["REQUEST_METHOD"] === 'POST'){
     $dados = $result->fetch_assoc();
     $stmt->close();
 
-    if ($dados){
+    if ($dados) {
         $_SESSION['user_id'] = $dados['id_usuario'];
         $_SESSION['email'] = $dados['email'];
         header('Location: dashboard.php');
@@ -27,19 +27,50 @@ if($_SERVER["REQUEST_METHOD"] === 'POST'){
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-</head>
-<body>
-    
-    <h3>Login</h3>
-    <form method="POST">
-        <input type="text" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Senha" required>
-        <button type="submit">Entrar</button>
-    </form>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+
+    <div class="container-fluid content">
+        <div class="row h-20">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid d-flex justify-content-center">
+                        <a class="navbar-brand fs-4" href="#"></a>
+                        <img src="assets/icon/logoSite.svg" width="auto" height="150" class="d-inline-block align-top" alt="">
+                    </div>
+                </nav>
+            </div>
+
+            <div class="col d-flex align-items-center justify-content-center">
+                <form class="loginForm">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control loginInput" id="email" placeholder="Email" aria-describedby="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Senha</label>
+                        <input type="password" class="form-control loginInput" id="senha" placeholder="Senha">
+                        <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn">Esqueci minha senha</button>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-light btnLogin">Entrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>
