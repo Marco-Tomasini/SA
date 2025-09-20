8dee6b34aaa643139b61c0c1c2065f7bc7b3bd0c
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-09-20 04:10:15 -0300

    UPDATE - Create 2.0

--- public/create.php
+++ public/create.php
@@ -5,13 +5,13 @@ include 'db.php';
 session_start();
 
 
-if(empty($_SESSION["user_id"])){
+if (empty($_SESSION["user_id"])) {
     header("Location: ../index.php");
     exit;
 };
 
 $register_msg = "";
-if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])) {
     $nome = $_POST['nome'] ?? "";
     $email = $_POST['email'] ?? "";
     $pass = $_POST['senha'] ?? "";
@@ -21,17 +21,17 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
     $nascimento = $_POST['nascimento'] ?? "";
     $endereco = $_POST['endereco'] ?? "";
     $contato = $_POST['contato'] ?? "";
-    if($nome && $pass){
-        $stmt = $mysqli -> prepare("INSERT INTO usuario (nome, email, senha, perfil, CPF, tipo_sanguineo, data_nascimento, endereco, contato) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt -> bind_param("sssssssss", $nome, $email, $pass, $perfil, $cpf, $sangue, $nascimento, $endereco, $contato);
+    if ($nome && $pass) {
+        $stmt = $mysqli->prepare("INSERT INTO usuario (nome, email, senha, perfil, CPF, tipo_sanguineo, data_nascimento, endereco, contato) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssssss", $nome, $email, $pass, $perfil, $cpf, $sangue, $nascimento, $endereco, $contato);
 
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             $register_msg = "Usuário cadastrado com sucesso!";
-        }else{
+        } else {
             $register_msg = "Erro ao cadastrar novo usuário.";
         };
         $stmt->close();
-    }else{
+    } else {
         $register = "Preencha todos os campos.";
     };
 };
@@ -39,60 +39,70 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['register'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
+
 <body>
-    
-    <form method="POST">
-        <label for="nome">Nome: </label>
-        <input type="text" name="nome" placeholder="Insira...">
+    <main>
+        <div class="container-fluid">
+            <form method="POST" class="p-5">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome: </label>
+                    <input type="text" name="nome" class="form-control" id="nome" placeholder="Insira...">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Insira...">
+                </div>
+                <div class="mb-3">
+                    <label for="senha" class="form-label">Senha: </label>
+                    <input type="password" class="form-control" id="senha" name="senha" placeholder="Insira...">
+                </div>
+                <div class="mb-3">
+                    <label for="perfil" class="form-label">Perfil: </label>
+                    <select class="form-select" name="perfil" id="perfil">
+                        <option value="controlador">Controlador</option>
+                        <option value="engenheiro">Engenheiro</option>
+                        <option value="planejador">Planejador</option>
+                        <option value="maquinista">Maquinista</option>
+                        <option value="gerente">Gerente</option>
+                    </select>
+                </div>
 
-        <label for="email">Email: </label>
-        <input type="text" name="email" placeholder="Insira...">
+                <div class="mb-3">
+                    <label for="cpf" class="form-label">CPF: </label>
+                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Insira...">
+                </div>
 
-        <label for="senha">Senha: </label>
-        <input type="password" name="senha" placeholder="Insira...">
+                <div class="mb-3">
+                    <label for="nascimento" class="form-label">Data Nascimento: </label>
+                    <input type="date" class="form-control" id="nascimento" name="nascimento" placeholder="Insira...">
+                </div>
 
-        <label for="perfil">Perfil: </label>
-        <select name="perfil" id="perfil">
-            <option value="controlador">Controlador</option>
-            <option value="engenheiro">Engenheiro</option>
-            <option value="planejador">Planejador</option>
-            <option value="maquinista">Maquinista</option>
-            <option value="gerente">Gerente</option>
-        </select>
+                <div class="mb-3">
+                    <label for="endereco" class="form-label">Endereço: </label>
+                    <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Insira...">  
+                </div>
 
-        <label for="cpf">CPF: </label>
-        <input type="text" name="cpf" placeholder="Insira...">
+                <div class="mb-3">
+                    <label for="contato" class="form-label">Contato: </label>
+                    <input type="text" class="form-control" id="contato" name="contato" placeholder="Insira...">
+                </div>
 
-        <label for="sangue">Tipo Sanguineo: </label>
-        <select name="sangue" id="sangue">
-            <option value="#" selected disabled>Escolha</option>
-            <option value="a+">A+</option>
-            <option value="a-">A-</option>
-            <option value="b+">B+</option>
-            <option value="b-">B-</option>
-            <option value="ab+">AB+</option>
-            <option value="ab-">AB-</option>
-            <option value="o+">O+</option>
-            <option value="o-">O-</option>
-            <option value="vermelinho">Vermelinho</option>
-        </select>
-
-        <label for="nascimento">Data Nascimento: </label>
-        <input type="date" name="nascimento" placeholder="Insira...">
-
-        <label for="endereco">Endereço: </label>
-        <input type="text" name="endereco" placeholder="Insira...">
-
-        <label for="contato">Contato: </label>
-        <input type="text" name="contato" placeholder="Insira...">
-
-        <button type="submit" name="register" value="1">Cadastrar</button>
-    </form>
-
+                <button type="submit" name="register" value="1" class="btn botaoCreate">Cadastrar</button>
+            </form>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
