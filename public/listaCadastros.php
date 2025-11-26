915d53f101d3bc4e05fd5f6b4a4f7e21bd27fbe1
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-11-26 11:46:05 -0300

    UPDATE: Front-end da lista de cadastro pronto

--- public/listaCadastros.php
+++ public/listaCadastros.php
@@ -13,29 +13,38 @@
 
 <body>
 
-    <div class="row headerDash d-flex align-items-center">
-        <div class="col-8  welcome lh-1">
-            <div class="col ms-4 fw-bold fs-5">
-                <p class="mb-0">Lista de Cadastros</p>
+    <div class="container-fluid">
+        <div class="row headerDash d-flex align-items-center">
+            <div class="col-8  welcome lh-1">
+                <div class="col ms-4 fw-bold fs-5 d-flex align-items-center">
+                    <i class="bi bi-box-arrow-in-left fs-3 me-3" onclick="window.location.href='dashboard.php'"></i>
+                    <p class="mb-0">Lista de Cadastros</p>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="col d-flex align-items-center justify-content-end">
+                    <?php include 'partials/sidebar.php'; ?>
+                </div>
             </div>
         </div>
 
-        <div class="col-4">
-            <div class="col d-flex align-items-center justify-content-end">
-                <?php include 'partials/sidebar.php'; ?>
+        <div class="row row-cols-1 d-flex flex-column align-items-center justify-content-center gap-4 mt-5">
+            <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 d-flex justify-content-between">
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createTrem.php'">Cadastro de Trem</Button>
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createAlerta.php'">Cadastro de Alerta</Button>
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='cadastrorotas.php'">Cadastro de Rotas</Button>
+            </div>
+            <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 d-flex justify-content-between">
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createViagem.php'">Cadastro de Viagem</Button>
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createEstacao.php'">Cadastro de Estações</Button>
+            </div>
+            <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 d-flex justify-content-between">
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createOrdemM.php'">Cadastro de Ordem de Manutenção</Button>
+                <Button class="btn btn-dark btnSidebar custom-height-btn fw-semibold" onclick="location.href='createSegmentoRota.php'">Cadastro de Segmento de Rota</Button>
             </div>
         </div>
     </div>
-
-    <button type="button" class="btn me-4"><img src="../assets/icon/seta-curva-esquerda 1.png" alt="" onclick="location.href='dashboard.php'"></button>
-
-    <Button onclick="location.href='cadastrorotas.php'">Cadastro de Rotas</Button>
-    <Button onclick="location.href='createEstacao.php'">Cadastro de Estações</Button>
-    <Button onclick="location.href='createTrem.php'">Cadastro de Trem</Button>
-    <Button onclick="location.href='createViagem.php'">Cadastro de Viagem</Button>
-    <Button onclick="location.href='createSegmentoRota.php'">Cadastro de Segmento de Rota</Button>
-    <Button onclick="location.href='createAlerta.php'">Cadastro de Alerta</Button>
-    <Button onclick="location.href='createOrdemM.php'">Cadastro de Ordem de Manutenção</Button>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
