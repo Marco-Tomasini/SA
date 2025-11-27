0e8e990b7912a3e620e85064995df57eeef8fb1a
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-11-27 19:57:58 -0300

    UPDATE - caso não esteja no servidor local automaticamente tenta conexão pelo servidor

--- public/db.php
+++ public/db.php
@@ -13,3 +13,17 @@
         echo "Coneção Falhou, erro: " . $e->getMessage();
 
     }
+    
+    if (!isset($conn) || !$conn instanceof PDO) {
+        try {
+            $servername = "sql305.infinityfree.com";
+            $username   = "if0_40538950";
+            $password   = "marconic0712";
+            $dbname     = "if0_40538950_smartcitiesv11";
+
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Conexão alterna (InfinityFree) falhou: " . $e->getMessage();
+        }
+    }
