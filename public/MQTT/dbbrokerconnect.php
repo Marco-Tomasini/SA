574cbe62719c8c26cadf72e97e287dd814daf3d2
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-11-27 20:37:24 -0300

    UPDATE - broker conectando com banco conforme área de uso

--- public/MQTT/dbbrokerconnect.php
+++ public/MQTT/dbbrokerconnect.php
@@ -1,12 +1,25 @@
 <?php
+    $whitelist = array('127.0.0.1', '::1', 'localhost');
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "smartcitiesv11";
+    // Verifica se está rodando localmente ou no servidor
+    if (in_array($_SERVER['HTTP_HOST'], $whitelist)) {
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "root"; 
+        $dbname     = "smartcitiesv11";
+    } else {
+        $servername = "sql305.infinityfree.com";
+        $username   = "if0_40538950";
+        $password   = "marconic0712";
+        $dbname     = "if0_40538950_smartcitiesv11";
+    }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // Cria a conexão usando MySQLi
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Conexao falhou: " . $conn->connect_error);
-}
+    // Verifica se houve erro na conexão
+    if ($conn->connect_error) {
+        die("Erro na conexão: " . $conn->connect_error);
+    }
+    $conn->set_charset("utf8mb4");
+?>
