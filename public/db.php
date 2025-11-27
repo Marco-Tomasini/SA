52f40dfdea8662b52e10b44ca56371b1b9c1cca6
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-11-27 20:05:04 -0300

    UPDATE - verificando corretamente conexão local ou com servidor

--- public/db.php
+++ public/db.php
@@ -1,29 +1,24 @@
 <?php
+    $whitelist = array('127.0.0.1', '::1', 'localhost');
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "smartcitiesv11";
-
-    try{
-        $conn = new PDO("mysql: host=$servername; dbname=$dbname", $username, $password);
-
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e){
-        echo "Coneção Falhou, erro: " . $e->getMessage();
-
+    if (in_array($_SERVER['HTTP_HOST'], $whitelist)) {
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "root"; 
+        $dbname     = "smartcitiesv11";
+    } else {
+        $servername = "sql305.infinityfree.com";
+        $username   = "if0_40538950";
+        $password   = "marconic0712";
+        $dbname     = "if0_40538950_smartcitiesv11";
     }
-    
-    if (!isset($conn) || !$conn instanceof PDO) {
-        try {
-            $servername = "sql305.infinityfree.com";
-            $username   = "if0_40538950";
-            $password   = "marconic0712";
-            $dbname     = "if0_40538950_smartcitiesv11";
 
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "Conexão alterna (InfinityFree) falhou: " . $e->getMessage();
-        }
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    } catch(PDOException $e) {
+        echo "Erro na conexão: " . $e->getMessage();
+        exit;
     }
+?>
